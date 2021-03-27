8fcd154f32fa87dc9a6849ac6b2d1cd5733b2d3a
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2021-03-27 10:45:28 -0500

    Agregar archivos de proyecto.

---
+++ Demostracion/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Demostracion
+{
+    public class Program
+    {
+
+        //boolean 1 o 0
+        //if, ingles, SI
+        //else, ingles, SI NO
+        public static bool CalcularDiyuncion(bool proposicionP, bool proposicionQ)
+        {
+            if (proposicionP == false && proposicionQ == false)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool CalcularConjuncion(bool proposicionP, bool proposicionQ)
+        {
+            if (proposicionP == true && proposicionQ == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool CalcularNegacion(bool proposicionP)
+        {
+            return !proposicionP;// ! = ¬
+        }
+
+        public static bool CalcularCondicional(bool proposicionImplica, bool proposicionImplicada)// p->Q === ¬P v Q
+        {
+            return CalcularDiyuncion(CalcularNegacion(proposicionImplica), proposicionImplicada);
+        }
+
+        public static bool CalcularBiCondicional(bool proposicionP, bool proposicionQ)
+        {
+            if (proposicionP == proposicionQ)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            var proposicionP = false;
+            var proposicionQ = false;
+
+            var PyQ = CalcularConjuncion(proposicionP, proposicionQ);
+            var negacionDePyQ = CalcularNegacion(PyQ);
+            var PoQ = CalcularDiyuncion(proposicionP, proposicionQ);
+            var QimplicaP = CalcularCondicional(proposicionQ, proposicionP);
+            var PoQEquivaleQimplicaP = CalcularBiCondicional(PoQ, QimplicaP);
+            var resultado = CalcularCondicional(negacionDePyQ, PoQEquivaleQimplicaP);
+
+            Console.WriteLine("¬(P^Q) => ((PvQ) <=> (Q=>P)): " + (resultado ? "V" : "F"));
+
+            Console.ReadLine();
+        }
+    }
+}
